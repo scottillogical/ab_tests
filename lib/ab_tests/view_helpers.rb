78357e2d4a99493eb40f5ac4360d343f864998ff
Scott Schulthess <scottschulthess@gmail.com>
2013-05-30 16:44:30 -0400

    a bug was created by the capture command working in rails env vs pure ruby gem env

--- lib/ab_tests/view_helpers.rb
+++ lib/ab_tests/view_helpers.rb
@@ -5,11 +5,19 @@ module AbTests
     def ab_test(test_name, variation_names, unique_identifier,  &block)
       unique_id_field = AbTests.configuration.unique_identifier
       if previous_variation = AbTests::Variation.where(unique_id_field => unique_identifier, test_name: test_name).first and unique_identifier
-        yield
+        unless Rails.env.test? # a block in a rails view
+          return capture(previous_variation.chosen_variation, &block)
+        else
+          yield(previous_variation.chosen_variation)
+        end
       end
       chosen_variation = variation_names.sample
       AbTests::Variation.create(test_name: test_name, chosen_variation: chosen_variation, unique_id_field => unique_identifier)
-      yield
+      unless Rails.env.test? # a block in a rails view
+        return capture(chosen_variation, &block)
+      else
+        yield(chosen_variation)
+      end
     end
   end
 end
